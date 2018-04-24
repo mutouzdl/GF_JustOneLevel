2eb8c4c94e3c5399f2a380e44e629e6ad8454894
钟迪龙 <musicvs@163.com>
2018-04-24 17:11:50 +0800

    导入地形资源和主角模型

---
+++ Assets/GF_JustOneLevel/Character/cha_data/Script/CharaView.js
@@ -0,0 +1,50 @@
+﻿#pragma strict
+var roteSpeed:float = 0.0;
+
+var animationSpeed:float =1.0;
+private var animationCount:uint;
+private var animationList:Array;
+function Start () {
+     print("animationGetCount:" + GetComponent.<Animation>().GetClipCount());
+     print(GetComponent.<Animation>().clip.name);
+     animationCount = GetComponent.<Animation>().GetClipCount();
+     print(gameObject.GetComponent.<Animation>());
+     animationList = GetAnimationList();
+}
+
+function Update () {
+  
+     transform.eulerAngles.y += roteSpeed;
+
+}
+
+function OnGUI (){
+     var margin : int = 10;
+
+     //Buttons
+     var buttonSpace:int = 25;
+     var rectWidth:int = 100;
+     var rectHeight:int = 40;
+     var max:int = 10;
+     var rects:Array = new Array();
+     var i:int = 0;
+
+     for (var name : String in animationList)
+     {
+          var rect:Rect = Rect(15,margin + 20*i + buttonSpace*i, rectWidth,rectHeight);
+          if(GUI.Button(rect,animationList[i].ToString())){
+               GetComponent.<Animation>().CrossFade(animationList[i],0.01);
+          }
+          i++;
+     }
+}
+
+private function GetAnimationList():Array
+{
+     var tmpArray = new Array();
+     for (var state : AnimationState in gameObject.GetComponent.<Animation>())
+     {
+          tmpArray.Add(state.name);
+     }
+     return tmpArray;
+}
---
+++ Assets/GF_JustOneLevel/Character/cha_data/Script/ScreenShot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShot : MonoBehaviour {
+	string format;
+	
+	// Use this for initialization
+	void Start () {
+		format = "yyyy-MM-dd-HH-mm-ss"; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			ScreenCapture.CaptureScreenshot(Application.dataPath + "/" + System.DateTime.Now.ToString(format) +".png");
+		}
+	}
+}
