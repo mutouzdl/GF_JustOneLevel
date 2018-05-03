c0a920a2591e47f11ed305ba34fc624f92f1109d
钟迪龙 <musicvs@163.com>
2018-05-03 16:23:22 +0800

    加入临时的攻击按钮

--- Assets/GF_JustOneLevel/Scripts/Procedure/ProcedureGame.cs
+++ Assets/GF_JustOneLevel/Scripts/Procedure/ProcedureGame.cs
@@ -7,8 +7,9 @@ using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedure
 using System;
 
 public class ProcedureGame : ProcedureBase {
-	private SurvivalGame m_SurvivalGame = null;
-	private float time = 0;
+    private SurvivalGame m_SurvivalGame = null;
+    private UIPlayerOperate m_UIPlayerOperate = null;
+    private float time = 0;
 
     protected override void OnInit (ProcedureOwner procedureOwner) {
         base.OnInit (procedureOwner);
@@ -16,16 +17,38 @@ public class ProcedureGame : ProcedureBase {
         m_SurvivalGame = new SurvivalGame ();
     }
 
-	protected override void OnEnter (ProcedureOwner procedureOwner) {
-		base.OnEnter (procedureOwner);
+    protected override void OnEnter (ProcedureOwner procedureOwner) {
+        base.OnEnter (procedureOwner);
 
-		m_SurvivalGame.Initialize();
-	}
+        m_SurvivalGame.Initialize ();
 
-	protected override void OnUpdate (ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds) {
+        // 加载UI
+        GameEntry.Event.Subscribe (OpenUIFormSuccessEventArgs.EventId, OnOpenUIFormSuccess);
+
+        GameEntry.UI.OpenUIForm ("Assets/GF_JustOneLevel/Prefabs/UI/UIPlayerOperate.prefab", "DefaultGroup");
+    }
+
+    protected override void OnLeave (ProcedureOwner procedureOwner, bool isShutdown) {
+        base.OnLeave (procedureOwner, isShutdown);
+
+        GameEntry.Event.Unsubscribe (OpenUIFormSuccessEventArgs.EventId, OnOpenUIFormSuccess);
+
+        if (m_UIPlayerOperate != null) {
+            GameEntry.UI.CloseUIForm (m_UIPlayerOperate.UIForm);
+            m_UIPlayerOperate = null;
+        }
+    }
+
+    protected override void OnUpdate (ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds) {
         if (m_SurvivalGame != null && !m_SurvivalGame.GameOver) {
             m_SurvivalGame.Update (elapseSeconds, realElapseSeconds);
             return;
         }
-	}
+    }
+
+    private void OnOpenUIFormSuccess (object sender, GameEventArgs e) {
+        OpenUIFormSuccessEventArgs ne = (OpenUIFormSuccessEventArgs) e;
+
+        m_UIPlayerOperate = (UIPlayerOperate) ne.UIForm.Logic;
+    }
 }
---
+++ Assets/GF_JustOneLevel/Scripts/UI/UIPlayerOperate.cs
@@ -0,0 +1,11 @@
+using GameFramework;
+using UnityGameFramework.Runtime;
+
+public class UIPlayerOperate : UIFormLogic {
+    /// <summary>
+    /// 玩家点击攻击按钮
+    /// </summary>
+    public void OnAtkClick() {
+        Log.Info("OnAtkClick");
+    }
+}
